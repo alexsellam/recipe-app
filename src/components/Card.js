8f3af556738a0a6c372962449fcd5a2cf0ce3dec
alexsellam <alex.sellam@gmail.com>
2019-02-01 17:45:02 +0100

    dynamic image prevent user error

--- src/components/Card.js
+++ src/components/Card.js
@@ -7,11 +7,19 @@ const Card = ({ details }) => {
 
         const instructions = details.instructions
         .split('\n')
-        .map(item => <li key={item}>  {item} </li>)    
+        .map(item => <li key={item}>  {item} </li>) 
+        
+        const requireImage = chemin => {
+            try {
+                return require(`../img/${chemin}`)
+            } catch (err) {
+                return require(`../img/default.jpeg`)
+            }
+        }
     return (
         <div className='card'>
             <div className="image">
-                <img src={require(`../img/${details.image}`)} alt={details.nom} />
+                <img src={requireImage(details.image)} alt={details.nom} />
             </div>
             <div className='recette'>
                 <h2> {details.nom}</h2>
